5b2b12d010111fe3b07c00530822d924f497ca83
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-19 18:05:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-def carica_immagini_da_excel(file_path, sheet_name, colonna_immagini):
+def carica_immagini_da_excel(file_path):
     try:
-        df = pd.read_excel(file_path, sheet_name=sheet_name)
+        xls = pd.ExcelFile(file_path)
         images = []
-        for index, row in df.iterrows():
-            if colonna_immagini in row and isinstance(row[colonna_immagini], bytes):
-                try:
-                    img = Image.open(io.BytesIO(row[colonna_immagini]))
-                    images.append(img)
-                except Exception as e:
-                    st.warning(f"Impossibile caricare l'immagine dalla riga {index}: {e}")
+        for sheet_name in xls.sheet_names:
+            df = pd.read_excel(xls, sheet_name=sheet_name)
+            for col in df.columns:
+                for index, row in df.iterrows():
+                    if isinstance(row[col], bytes):
+                        try:
+                            img = Image.open(io.BytesIO(row[col]))
+                            images.append(img)
+                        except Exception as e:
+                            st.warning(f"Impossibile caricare l'immagine dalla riga {index} del foglio di lavoro '{sheet_name}' e colonna '{col}': {e}")
         return images
     except Exception as e:
         st.error(f"Si è verificato un errore durante il caricamento del file Excel: {e}")
@@ -25,12 +28,8 @@ st.title("Tabulazione JOOR")
 uploaded_file = st.file_uploader("Carica il file Excel", type=['xlsx'])
 
 if uploaded_file is not None:
-    # Imposta il nome del foglio di lavoro e la colonna in cui cercare le immagini
-    sheet_name = "PO# 15289031"  # Inserisci il nome del foglio di lavoro
-    colonna_immagini = "A"  # Inserisci il nome della colonna delle immagini
-    
     # Carica le immagini dal file Excel
-    images = carica_immagini_da_excel(uploaded_file, sheet_name, colonna_immagini)
+    images = carica_immagini_da_excel(uploaded_file)
 
     if images:
         st.header("Anteprima Immagini")
