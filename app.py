27fc5f0e2ee92647ef01ebace52fd810aa6d8df4
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-19 11:34:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ def is_numeric_column(col):
         return False
 
 def extract_numeric_part(col):
-    # Questa versione assume che col sia sempre una stringa contenente un numero
-    # o una stringa che può essere convertita in numero.
+    # Assicurati che col sia una stringa
+    col_str = str(col)
     try:
         # Estrai solo i numeri (e il punto per i decimali) dall'etichetta della colonna
-        numeric_part = ''.join(filter(str.isdigit, col)) or '0'
+        numeric_part = ''.join(filter(str.isdigit, col_str)) or '0'
         return int(numeric_part)
     except ValueError:
         # In caso di qualsiasi errore, restituisci un numero molto alto per posizionare la colonna alla fine
