81c51489c81194a8aa7a28fdcfa9d50c952287a3
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-19 12:32:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,15 +42,22 @@ def is_numeric_column(col):
         return False
 
 def extract_numeric_part(col):
-    # Assicurati che col sia una stringa
+    # Assegna un peso ai caratteri non numerici
+    weight = 1
+    for char in str(col):
+        if not char.isdigit():
+            weight += 10  # Modifica il peso in base alle tue esigenze
+    
     col_str = str(col)
-    try:
-        # Estrai solo i numeri (e il punto per i decimali) dall'etichetta della colonna
-        numeric_part = ''.join(filter(str.isdigit, col_str)) or '0'
-        return int(numeric_part)
-    except ValueError:
-        # In caso di qualsiasi errore, restituisci un valore intermedio per posizionamento approssimativo
-        return len(col_str)  # Adjust weight as needed
+    numeric_part = ''.join(filter(str.isdigit, col_str)) or '0'
+    return int(numeric_part) * weight
+
+# Ordina in ordine decrescente
+size_cols.sort(key=lambda col: extract_numeric_part(str(col)), reverse=True)
+
+# Escludi la riga "Total"
+if "Total:" in combined_df.values:
+    combined_df = combined_df[combined_df != "Total:"]
 
 def save_combined_data_to_excel(cleaned_data):
     combined_df = pd.DataFrame()
