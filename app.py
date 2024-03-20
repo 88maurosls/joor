83b2c2a874592ea3a4b3ce51366777dc2b0681b3
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-03-20 10:34:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 import base64
-import os
-from openpyxl import Workbook
-from openpyxl.styles import PatternFill
 
 def trova_indice_intestazione(df):
     for index, row in df.iterrows():
@@ -76,53 +73,22 @@ def estrai_e_riordina_dati_da_tutti_sheet(file_path):
 
     return all_extracted_data
 
-def main():
-    st.title("Caricamento e elaborazione file Excel")
-
-    uploaded_file = st.file_uploader("Carica il file Excel", type=["xlsx"])
-
-    if uploaded_file is not None:
-        # Lettura e elaborazione dei dati
-        all_extracted_data = estrai_e_riordina_dati_da_tutti_sheet(uploaded_file)
-
-        # Rimozione delle colonne "Style Image" e formattazione delle colonne delle taglie
-        all_extracted_data.drop(columns=['Style Image'], inplace=True)
-
-        # Rimuovi le colonne delle taglie che hanno solo valori zero
-        all_extracted_data = all_extracted_data.loc[:, (all_extracted_data != 0).any(axis=0)]
-
-        # Converti il DataFrame in HTML e visualizzalo
-        st.write(all_extracted_data.to_html(escape=False), unsafe_allow_html=True)
-
-        # Download del file elaborato
-        output_file_path = "extracted_data.xlsx"
-        all_extracted_data.to_excel(output_file_path, index=False)
-        apply_conditional_formatting(output_file_path, all_extracted_data)
-        st.markdown(get_binary_file_downloader_html(output_file_path, 'Excel file'), unsafe_allow_html=True)
-
-def apply_conditional_formatting(file_path, df):
-    wb = Workbook()
-    ws = wb.active
-
-    for row in df.iterrows():
-        row_index, row_data = row
-        ws.append(row_data.tolist())
-
-    yellow_fill = PatternFill(start_color='FFFF99', end_color='FFFF99', fill_type='solid')
-
-    for row in ws.iter_rows(min_row=2, max_row=ws.max_row, min_col=6, max_col=ws.max_column):
-        for cell in row:
-            if cell.value != 0:
-                cell.fill = yellow_fill
-
-    wb.save(file_path)
-
 def get_binary_file_downloader_html(bin_file, file_label='File'):
     with open(bin_file, 'rb') as f:
         data = f.read()
     bin_str = base64.b64encode(data).decode()
-    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">{file_label}</a>'
+    href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{file_label}">{file_label}</a>'
     return href
 
-if __name__ == "__main__":
-    main()
+st.title("Caricamento e elaborazione file Excel")
+
+uploaded_file = st.file_uploader("Carica il file Excel", type=["xlsx"])
+
+if uploaded_file is not None:
+    # Lettura e elaborazione dei dati
+    all_extracted_data = estrai_e_riordina_dati_da_tutti_sheet(uploaded_file)
+
+    # Rimuovi le colonne delle taglie che hanno solo valori zero
+    first_size_col_idx = all_extracted_data.columns.get_loc("Country of Origin") + 1
+    last_size_col_idx = all_extracted_data.columns.get_loc("Sugg. Retail (EUR)") - 1
+    colonne_da_rimuovere = [col for col in all_extracted_data.columns[first_size_col_idx:last_size_col
